abf54102aef781ad9ff2f3b80dac8cda19d79ceb
Ros <123762126+RosalindHook@users.noreply.github.com>
2023-02-03 14:54:23 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-// jobGenerator function
-function randomJob() {
+// welcome function
+function welcome() {
     let name = prompt("What is your name?");
     alert("Hello, " + name + "! I think you've got a great career ahead of you in science!");
 }
 
 //event listener
-let jobGeneratorButton = document.getElementByID("job-generator-button");
-jobGeneratorButton.addEventListener("click", randomJob);
+let welcomeFutureButton = document.getElementById("welcome-button");
+welcomeFutureButton.addEventListener("click", welcome);
 
 
+
+
+var button = document.getElementById("job-generator-button");
+var myarray = ['biochemist', 'cartographer', 'climate scientist', 'data analyst', 'forensic scientist', 'hydrologist',
+'economist', 'meteorologist', 'hydrologist', 'marine engineer', 'software engineer', 'zoologist', 'astronaut', 'physicist',
+'robotics engineer', 'software engineer', 'web developer'];
+
+button.onclick = function() {
+    alert("Hey there, you'd make a great " + (myarray[Math.floor(Math.random() * myarray.length)]) + "... just saying.");
+};
