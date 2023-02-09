02c333a74077d6204a534b5e2899436bed927d90
aliyahru <115578574+aliyahru@users.noreply.github.com>
2023-02-09 12:06:45 +0000

    Update animPercent.js

--- animPercent.js
+++ animPercent.js
@@ -1,13 +1,13 @@
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-  if (document.documentElement.scrollTop > 200) {
+  if (document.documentElement.scrollTop > 300) {
     document.getElementById("stat1").className = "slide1";
   }
-  if (document.documentElement.scrollTop > 485) {
+  if (document.documentElement.scrollTop > 585) {
     document.getElementById("stat2").className = "slide2";
   }
-  if (document.documentElement.scrollTop > 650) {
+  if (document.documentElement.scrollTop > 750) {
     document.getElementById("stat3").className = "slide3";
   }
 }
